2afc1d83e03d25b6a168ec33110a0e3e3b9239da
Florencia Acuña <110423383+floracu20@users.noreply.github.com>
2024-07-17 14:12:13 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,78 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#excusa").innerHTML = generarExcusa();
+};
+
+let generarExcusa = () => {
+  let pronombre = ["A", "The", "My", "Your"];
+  let sujeto = [
+    "dog",
+    "cat",
+    "fish",
+    "parrot",
+    "hamster",
+    "P.E teacher",
+    "psychologist",
+    "therapist",
+    "colleague"
+  ];
+  let verbo = [
+    "ate my",
+    "stole my",
+    "lost my",
+    "bought my",
+    "laughed at my",
+    "sold my",
+    "cried looking at my",
+    "ruined my",
+    "forgot my",
+    "spat on my"
+  ];
+  let objeto = [
+    "homework",
+    "food",
+    "car",
+    "project",
+    "gift",
+    "clock",
+    "computer",
+    "book",
+    "phone",
+    "breakfast"
+  ];
+  let cuando = [
+    "today",
+    "yesterday",
+    "last night",
+    "this morning",
+    "last week",
+    "last Sunday night",
+    "five minutes ago",
+    "just before I came here",
+    "last Monday",
+    "last Sunday"
+  ];
+
+  let pronombreIndex = Math.floor(Math.random() * pronombre.length);
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let verboIndex = Math.floor(Math.random() * verbo.length);
+  let objetoIndex = Math.floor(Math.random() * objeto.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+  return (
+    pronombre[pronombreIndex] +
+    " " +
+    sujeto[sujetoIndex] +
+    " " +
+    verbo[verboIndex] +
+    " " +
+    objeto[objetoIndex] +
+    " " +
+    cuando[cuandoIndex] +
+    "!"
+  );
 };
